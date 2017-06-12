6e0f72a361305e6da3b987c48779bd9bbecd3ee1
tongdou <shenyuzhu278@sina.com>
2017-06-12 21:09:43 +0800

    create class by CGLib, test PermSize and MaxPermSize

--- jvm/clazz/src/main/java/com/tongdou/jvm/gc/CGLIB.java
+++ jvm/clazz/src/main/java/com/tongdou/jvm/gc/CGLIB.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class CGLIB {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10; i++) {
             createObject();
             System.out.println(i);
         }
