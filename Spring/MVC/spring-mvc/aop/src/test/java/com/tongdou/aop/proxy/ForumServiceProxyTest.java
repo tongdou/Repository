7acf276aeb3ef5151e01820983cba6a89b717b33
tongdou <shenyuzhu278@sina.com>
2017-08-05 22:27:17 +0800

    test proxy aop

---
+++ Spring/MVC/spring-mvc/aop/src/test/java/com/tongdou/aop/proxy/ForumServiceProxyTest.java
@@ -0,0 +1,31 @@
+package com.tongdou.aop.proxy;
+
+import com.tongdou.aop.withoutaop.ForumService;
+import com.tongdou.aop.withoutaop.ForumServiceImpl;
+import org.junit.Test;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Test forum proxy
+ * Created by shenyuzhu on 2017/8/5.
+ */
+public class ForumServiceProxyTest {
+
+    @Test
+    public void proxyTest() {
+        ForumService service = new ForumServiceProxyImpl();
+
+
+        // create proxy
+        PerformanceHandler handler = new PerformanceHandler(service);
+        ForumService proxy = (ForumService) Proxy.newProxyInstance(service.getClass().getClassLoader(), service.getClass().getInterfaces(),
+                handler);
+
+        // invoke
+        proxy.addTopic("乐视");
+
+
+    }
+
+}
