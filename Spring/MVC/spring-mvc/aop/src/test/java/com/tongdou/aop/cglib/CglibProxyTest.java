c10a7f6d5d10a162f2c321f6846af29095891886
tongdou <shenyuzhu278@sina.com>
2017-08-08 22:29:24 +0800

    proxy by cglib

---
+++ Spring/MVC/spring-mvc/aop/src/test/java/com/tongdou/aop/cglib/CglibProxyTest.java
@@ -0,0 +1,32 @@
+package com.tongdou.aop.cglib;
+
+import com.tongdou.aop.proxy.ForumServiceProxyImpl;
+import com.tongdou.aop.withoutaop.ForumService;
+import com.tongdou.aop.withoutaop.ForumServiceImpl;
+import net.sf.cglib.core.DebuggingClassWriter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test cglib proxy
+ * Created by shenyuzhu on 2017/8/8.
+ */
+public class CglibProxyTest {
+    private Log log = LogFactory.getLog(CglibProxy.class);
+
+    @Test
+    public void testCglibProxy() {
+        // 输出Cglib生产类的class字节码
+        log.info(String.format("class path %s", Class.class.getResource("/").getPath()));
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, Class.class.getResource("/").getPath());
+
+        CglibProxy proxy = new CglibProxy();
+        ForumServiceProxyImpl service = (ForumServiceProxyImpl) proxy.getProxy(ForumServiceProxyImpl.class);
+        service.addTopic("cglib topic");
+
+    }
+
+}
