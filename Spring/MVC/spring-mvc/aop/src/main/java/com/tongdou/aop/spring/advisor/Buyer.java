2eaa255e0f8408bccad9ba82d614c71e14629dc7
tongdou <shenyuzhu278@sina.com>
2017-08-21 21:40:29 +0800

    StaticMethodMatcherPointcutAdvisor

---
+++ Spring/MVC/spring-mvc/aop/src/main/java/com/tongdou/aop/spring/advisor/Buyer.java
@@ -0,0 +1,21 @@
+package com.tongdou.aop.spring.advisor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * bean
+ * Created by shenyuzhu on 2017/8/21.
+ */
+public class Buyer {
+    static final Logger logger = LoggerFactory.getLogger(Seller.class);
+
+    public void greetTo() {
+        logger.info("greetTo");
+    }
+
+    public void seviceTo() {
+        logger.info("serviceTo");
+    }
+
+}
---
+++ Spring/MVC/spring-mvc/aop/src/test/java/com/tongdou/aop/spring/advisor/staticadvisor/GreetingAdvisorTest.java
@@ -0,0 +1,29 @@
+package com.tongdou.aop.spring.advisor.staticadvisor;
+
+import com.tongdou.aop.spring.advisor.Buyer;
+import com.tongdou.aop.spring.advisor.Seller;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shenyuzhu on 2017/8/21.
+ */
+public class GreetingAdvisorTest {
+    @Test
+    public void staticAdvisor() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:advisor/static_advisor.xml");
+        Buyer buyer = (Buyer) context.getBean("buyerProxy");
+        buyer.greetTo();
+        buyer.seviceTo();
+
+        Seller seller = (Seller) context.getBean("sellerProxy");
+        seller.greetTo();
+        seller.seviceTo();
+
+
+    }
+
+}
